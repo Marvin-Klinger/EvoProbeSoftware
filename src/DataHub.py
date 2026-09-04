e93fbf9514928bcd77e2c1710a0078bf7a4bbda1
Philipp <philipp.greiner@uni-a.de>
2026-09-04 22:31:59 +0200

    - changed filestructure and added new DataFrames to accommodate for logging devices separately

--- src/DataHub.py
+++ src/DataHub.py
@@ -1,3 +1,4 @@
+import datetime
 import time
 
 from DataReader import DataReader
@@ -12,37 +13,45 @@ class DataHub:
 
     def __init__(self, measurement_devices: list[MeasurementDevice], save_path, controller):
         self.measurement_devices = measurement_devices
-        self.save_path = save_path
         self.controller = controller
-        self.intervall = 2
+        self.save_path = os.path.dirname(save_path)
+        self.save_path_extensions = [os.path.basename(save_path)]
         self.start_time = None
+        self.intervall = 2
 
-        columns = ["timestamp", "timedelta"]
-        for device in self.measurement_devices:
-            columns += device.logging_keys
-        self.df = pd.DataFrame(columns=columns)
-        self.df.to_csv(self.save_path, encoding="utf-8", index=False)
-        raw_path = os.path.join(os.path.dirname(self.save_path), "raw")
-        if not os.path.exists(raw_path):
-            os.mkdir(raw_path)
-
-        self.reader = DataReader(datahub=self,
-                                 measurement_devices=self.measurement_devices,
-                                 interval=2)
+        self.dfs = []
+        self.initialize_files()
 
         self.graph_queue = Queue()
-        plotting_keys = []
-        for device in self.measurement_devices:
-            plotting_keys += device.plotting_keys
+        plotting_keys = self.dfs[0].columns[2:]
         self.graph = LiveGraph(queue=self.graph_queue,
-                               df=self.df,
+                               df=self.dfs[0],
                                x_axis="timedelta",
                                y_axis=plotting_keys)
+
+    def initialize_files(self):
+        # File Management
+        self.save_path = os.path.join(self.save_path, str(datetime.date.today()))
+        count = 1
+        while os.path.exists(self.save_path + "_" + str(count)):
+            count += 1
+        self.save_path = self. save_path + "_" + str(count)
+        os.mkdir(self.save_path)
+        master_df = pd.DataFrame(columns=["timestamp", "timedelta"] +
+                                         [f"{i+1}_{d.logging_keys}" for i, d in enumerate(self.measurement_devices)])
+        self.dfs.append(master_df)
+        master_df.to_csv(os.path.join(self.save_path, self.save_path_extensions[0]), encoding="utf-8", index=False)
+
+        os.mkdir(os.path.join(self.save_path, "raw"))
+        for i, device in enumerate(self.measurement_devices):
+            self.save_path_extensions.append(os.path.join("raw", f"{i+1}-{device.name}.csv"))
+            raw_df = pd.DataFrame(columns=["timestamp", "timedelta"] + device.logging_keys)
+            self.dfs.append(raw_df)
+            raw_df.to_csv(os.path.join(self.save_path, self.save_path_extensions[i+1]), encoding="utf-8", index=False)
 
     # starts the logging process and graph
     def start_logging(self):
         self.start_time = time.monotonic()
-        self.reader.start()
         self.graph.start()
         for device in self.measurement_devices:
             device.start_logging(self)
